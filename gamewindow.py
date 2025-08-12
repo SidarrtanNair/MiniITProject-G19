dc366bb60ffc32f6620e354b80c086521abb5181
IMRAN <imrandanialdezmin@gmail.com>
2025-08-12 22:58:39 +0800

    Window

---
+++ gamewindow.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Campus To Cosmos")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(("white"))
+    pygame.display.flip()
+
+pygame.quit()
