ffdcca768958edba0f3ab2fd9a5266d94e89f070
IMRAN <imrandanialdezmin@gmail.com>
2025-08-19 01:00:33 +0800

    it doesnt work dont pull

--- Map/gamewindow.py
+++ Map/gamewindow.py
@@ -1,5 +1,14 @@
-import pygame
-import sys
+import pygame,sys,os,random
+
+from tiles import *
+
+spritesheet = ('1.png')
+
+
+
+map = TileMap('background.csv',spritesheet)
+
+
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -8,6 +17,7 @@ pygame.display.set_caption("Campus To Cosmos")
 clock = pygame.time.Clock()
 
 while True:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -16,3 +26,7 @@ while True:
     screen.fill(("white"))
     pygame.display.update()
 
+    screen.fill(0,180,240)
+    map.draw_map(screen)
+    screen.blit(screen,(0,0))
+    pygame.display.update()
