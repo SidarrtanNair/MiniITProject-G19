a38b9f568a533a9c6b14effa8be1c5464c1a9b02
Michael Luke <mikeagentkiller@gmail.com>
2025-09-17 01:45:50 +0800

    working on the death screen

--- something/Michael's Work for Python/deathscreen.py
+++ something/Michael's Work for Python/deathscreen.py
@@ -4,11 +4,11 @@ pygame.init()
 
 info = pygame.display.Info()
 WIDTH, HEIGHT = info.current_w, info.current_h
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Death Screen')
 fps = 60
 clock = pygame.time.Clock()
-bg_deathscreen = pygame.image.load("cosmos3rd.jpg")
+bg_deathscreen = pygame.image.load("Doors_DS.jpg")
 bg_deathscreen = pygame.transform.scale(bg_deathscreen, (WIDTH, HEIGHT))
 font = pygame.font.Font(None, 120)
 small_font = pygame.font.Font(None, 60)
@@ -16,15 +16,12 @@ small_font = pygame.font.Font(None, 60)
 STATE = "death"
 
 def draw_death():
-    screen.fill("black")
-    text = font.render("Ain't no way bro just died", True, "red")
-    screen.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - 200))
+    screen.blit(bg_deathscreen, (0,0))
+    retry_btn = pygame.draw.rect(screen, 'light gray', [WIDTH//4 - 200, HEIGHT//2 + 200, 400, 80], 0, 5)
+    quit_btn = pygame.draw.rect(screen, 'light gray', [WIDTH//1.3 - 200, HEIGHT//2 + 200 , 400, 80], 0, 5)
 
-    retry_btn = pygame.draw.rect(screen, 'light gray', [WIDTH//2 - 200, HEIGHT//2, 400, 80], 0, 5)
-    quit_btn = pygame.draw.rect(screen, 'light gray', [WIDTH//2 - 200, HEIGHT//2 + 120, 400, 80], 0, 5)
-
-    screen.blit(small_font.render("Respawn", True, "black"), (WIDTH//2 - 60, HEIGHT//2 + 10))
-    screen.blit(small_font.render("Back To Menu", True, "black"), (WIDTH//2 - 100, HEIGHT//2 + 130))
+    screen.blit(small_font.render("Respawn", True, "black"), (WIDTH//4 - 75, HEIGHT//2 + 225))
+    screen.blit(small_font.render("Back To Menu", True, "black"), (WIDTH//1.3 - 150, HEIGHT//2 + 225))
 
     if retry_btn.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
         return "respawn"
@@ -43,10 +40,8 @@ while run:
             run = False
 
     if STATE == "respawn":
-        print("respawn pressed")
         run = False
-    elif STATE == "quit":
-        print("Quit pressed")
+    elif STATE == "menu":
         run = False
 
     pygame.display.flip()
